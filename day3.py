9ef3ebc084bf220af1671120cf679e38ee1a2399
Keelan <keelanpattersoncaesar@gmail.com>
2026-05-06 18:25:13 -0700

    pizza ordering system that calculates the final bill based on the size of the pizza and additional toppings. The user is prompted to input their choices, and the program calculates the total cost accordingly.

--- day3.py
+++ day3.py
@@ -33,9 +33,56 @@
 # except ValueError:
 #     print("That is not a number, silly.")
 
-user_input = input("Pick a number and I'll tell you if it's even or odd.\n")
-even_or_odd = int(user_input) % 2
-if even_or_odd == 0:
-    print("It's even.\n")
+# user_input = input("Pick a number and I'll tell you if it's even or odd.\n")
+# even_or_odd = int(user_input) % 2
+# if even_or_odd == 0:
+#     print("It's even.\n")
+# else: 
+#     print("Odd.")
+
+bill = 0
+print("Welcome to Python Pizza Deliveries!")
+
+# todo: work out how much they need to pay based on their size, pepperoni, and extra cheese choices.
+while True:
+    size = input("What size pizza do you want? Please choose S, M, or L: \n").upper()
+    if size == "S" or size == "M" or size == "L":
+        break
+    else:
+        print("Please choose S, M, or L. \n")
+
+while True:
+    pepperoni = input("Do you want pepperoni? Y or N: \n").upper()
+    if pepperoni == "Y" or pepperoni == "N":
+        break
+    else:
+        print("Please choose Y or N. \n")
+
+
+while True:
+    extra_cheese = input("Would you like extra cheese? Y or N: \n").upper()
+    if extra_cheese == "Y" or extra_cheese == "N":
+        break
+    else:
+        print("Please choose Y or N. \n")
+
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+
+if pepperoni == "Y" and size == "S":
+    bill += 2
+elif pepperoni == "Y" and (size == "M" or size == "L"):
+    bill += 3
 else: 
-    print("Odd.")
+    bill += 0
+
+if extra_cheese == "Y":
+    bill += 1
+else:
+    bill += 0
+
+print(f"Your final bill is: ${bill:.2f}.")
